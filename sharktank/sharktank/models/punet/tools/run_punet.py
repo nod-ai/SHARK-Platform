92c23981a4452adf5cf594a2935a081ab6ece704
Stella Laurenzo <stellaraccident@gmail.com>
2024-07-08 18:38:30 -0700

    Bring punet/ref runner into alignment and update comparison report with more stats.

--- sharktank/sharktank/models/punet/tools/run_punet.py
+++ sharktank/sharktank/models/punet/tools/run_punet.py
@@ -14,47 +14,7 @@ from shark_turbine import aot
 from ..model import Unet2DConditionModel, ClassifierFreeGuidanceUnetModel
 from ....utils.patching import SaveModuleResultTensorsPatch
 
-
-def get_random_inputs(dtype, device):
-    torch.random.manual_seed(42)
-    max_length = 64
-    height = 1024
-    width = 1024
-    bs = 1
-    return {
-        "sample": torch.rand(bs, 4, height // 8, width // 8, dtype=dtype).to(device),
-        "timestep": torch.zeros(1, dtype=torch.int32).to(device),
-        "encoder_hidden_states": torch.rand(2 * bs, max_length, 2048, dtype=dtype).to(
-            device
-        ),
-        "text_embeds": torch.rand(2 * bs, 1280, dtype=dtype).to(device),
-        "time_ids": torch.zeros(2 * bs, 6, dtype=dtype).to(device),
-        "guidance_scale": torch.tensor([7.5], dtype=dtype).to(device),
-    }
-
-
-def load_inputs(st_path: Path, dtype, device):
-    from safetensors import safe_open
-
-    with safe_open(st_path, framework="pt", device=device) as st:
-        random_inputs = get_random_inputs(dtype=dtype, device=device)
-        inputs = {}
-        for name, random_input in random_inputs.items():
-            if name in st.keys():
-                print(f"Using specified input for tensor {name}")
-                t = st.get_tensor(name)
-                inputs[name] = t
-            else:
-                print(f"Using default/random tensor for tensor {name}")
-                inputs[name] = random_input
-    return inputs
-
-
-def save_outputs(st_path: Path, outputs):
-    from safetensors.torch import save_file
-
-    tensors = {str(i): t for i, t in enumerate(outputs)}
-    save_file(tensors, st_path)
+from .sample_data import get_random_inputs, load_inputs, save_outputs
 
 
 def main(argv):
