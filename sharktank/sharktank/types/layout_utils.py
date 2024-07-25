c1a634ef6cc31f5e69b4927c93f571443df40d68
Rob Suderman <rob.suderman@gmail.com>
2024-07-25 11:03:23 -0700

    Add support for fp8 saturation (#117)

--- sharktank/sharktank/types/layout_utils.py
+++ sharktank/sharktank/types/layout_utils.py
@@ -154,14 +154,20 @@ def saturate_cast(
     disable_saturate: bool = False,
 ) -> torch.Tensor:
     """Does a saturating cast to the given dtype. For floating point
-    values, this is a simple cast. For integer types, it will saturate to the
-    min/max range. An argument disable_saturate= is provided to allow
+    values, this is a simple cast except for fp8 which is saturated.
+    For integer types, it will saturate to the min/max range.
+    An argument disable_saturate= is provided to allow
     saturation to be disabled by flag without changing caller code. This is
     needed if (for example, trying to saturate a high precision integer
     type like int32) with a low precision tensor.
     """
     if dtype.is_floating_point:
+        finfo = torch.finfo(dtype)
+        isfp8 = finfo.bits == 8
+        if isfp8 and not disable_saturate:
+            t = t.clamp(finfo.min, finfo.max)
         return t.to(dtype=dtype)
+
     iinfo = torch.iinfo(dtype)
     if round_int:
         t = torch.round(t)
