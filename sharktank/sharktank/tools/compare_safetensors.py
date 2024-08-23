aea29f9b07ff174f174bd6c88da3a166fdf3a5d3
Kyle Herndon <kherndon@amd.com>
2024-08-23 07:36:36 +0000

    Adds additional analysis to the dump comparison tool for detecting different types of errors better

--- sharktank/sharktank/tools/compare_safetensors.py
+++ sharktank/sharktank/tools/compare_safetensors.py
@@ -19,6 +19,10 @@ class Reporter:
     def __init__(self, output_dir: Path):
         self.output_dir = output_dir
         self.output_dir.mkdir(parents=True, exist_ok=True)
+        self.differences_dir = output_dir / "differences"
+        self.differences_dir.mkdir(parents=True, exist_ok=True)
+        self.histograms_dir = output_dir / "histograms"
+        self.histograms_dir.mkdir(parents=True, exist_ok=True)
         self.counter = 0
         self.index = open(self.output_dir / "index.html", "wt")
         self.index.write("<html>\n")
@@ -28,6 +32,66 @@ class Reporter:
         self.index.write("</body>\n")
         self.index.write("</html>\n")
         self.index.close()
+
+    def overall_graph(self, stats_dict: dict):
+        f = self.index
+
+        xkey = list(stats_dict.keys())
+        xkey.sort()
+        ymax = []
+        ymean = []
+        ystddev = []
+        for xi in xkey:
+            rmax, rmean, rstddev = stats_dict[xi]
+            ymax.append(rmax)
+            ymean.append(rmean)
+            ystddev.append(rstddev)
+
+        dpi = 144
+        file_name_root = f"overall_"
+        x = torch.arange(0, len(xkey))
+
+        plot_filename_max = f"{file_name_root}_max.png"
+        fig, ax = plt.subplots()
+        ax.set_title(f"Overall maximum relative difference")
+        ax.plot(x, ymax)
+        ax.set(xlabel="dim", ylabel="rel diff")
+        ax.grid()
+        fig.set_figheight(2.5)
+        fig.set_figwidth(8)
+        fig.savefig(str(self.output_dir / plot_filename_max), dpi=dpi)
+        plt.close(fig)
+
+
+        plot_filename_mean = f"{file_name_root}_mean.png"
+        fig, ax = plt.subplots()
+        ax.set_title(f"Overall mean relative difference")
+        ax.plot(x, ymax)
+        ax.set(xlabel="dim", ylabel="mean rel diff")
+        ax.grid()
+        fig.set_figheight(2.5)
+        fig.set_figwidth(8)
+        fig.savefig(str(self.output_dir / plot_filename_mean), dpi=dpi)
+        plt.close(fig)
+
+
+        plot_filename_stddev = f"{file_name_root}_stddev.png"
+        fig, ax = plt.subplots()
+        ax.set_title(f"Overall standard deviation relative difference")
+        ax.plot(x, ymax)
+        ax.set(xlabel="dim", ylabel="stddev rel diff")
+        ax.grid()
+        fig.set_figheight(2.5)
+        fig.set_figwidth(8)
+        fig.savefig(str(self.output_dir / plot_filename_stddev), dpi=dpi)
+        plt.close(fig)
+        
+        f.write("<hr>\n")
+        f.write(f"<h3>Overall</h3>\n")
+        f.write(f"<img src='{plot_filename_max}'>\n")
+        f.write(f"<img src='{plot_filename_mean}'>\n")
+        f.write(f"<img src='{plot_filename_stddev}'>\n")
+        f.flush()
 
     def compare(self, name: str, expected: torch.Tensor, actual: torch.Tensor):
         f = self.index
@@ -62,6 +126,12 @@ class Reporter:
 
         f.write("<hr>\n")
         f.write(f"<h3>{name}</h3>\n")
+        
+        rel_error = torch.abs(diff_flat / exp_flat)
+        rel_max_error = torch.max(rel_error)
+        rel_mean, rel_stddev = torch.std_mean(rel_error)
+
+
         non_finite_count = (
             torch.nonzero(torch.logical_not(torch.isfinite(diff_flat)))
             .flatten()
@@ -93,16 +163,18 @@ class Reporter:
         )
 
         print(f"Generating plots {name}")
+        # Differences
         dpi = 144
         file_name_root = f"{self.counter}_{name}_"
-        plot_filename = f"{file_name_root}_diff.png"
+        plot_filename_diff = f"{file_name_root}_diff.png"
         self.counter += 1
         x = torch.arange(0, diff_flat.shape[0])
         y = diff_flat
 
         fig, ax = plt.subplots()
+        ax.set_title(f"Difference of {name}")
         ax.plot(x, y)
-        ax.set(xlabel="dim", ylabel="diff", title="Difference")
+        ax.set(xlabel="dim", ylabel="diff")
         ax.axhline(float(bound), linestyle=":", color="red", alpha=1.0)
         ax.axhline(float(-bound), linestyle=":", color="red", alpha=1.0)
         ax.axhline(float(diff_min), linestyle=":", color="blue", alpha=1.0)
@@ -110,12 +182,31 @@ class Reporter:
         ax.grid()
         fig.set_figheight(2.5)
         fig.set_figwidth(8)
-        fig.savefig(str(self.output_dir / plot_filename), dpi=dpi)
+        fig.savefig(str(self.differences_dir / plot_filename_diff), dpi=dpi)
         plt.close(fig)
 
-        f.write(f"<img src='{plot_filename}'>\n")
+        # Histogram
+        plot_filename_hist = f"{file_name_root}_hist.png"
+        x = torch.arange(0, diff_flat.shape[0])
+        y = torch.clamp(torch.nan_to_num(rel_error, 1000, 1000, 1000), 0, 2)
+
+        fig, ax = plt.subplots()
+        ax.hist(y, bins=torch.arange(0, 2, .02))
+        ax.set_title(f"Histogram of relative differences")
+        ax.set(xlabel="rel diff", ylabel="count")
+        ax.grid()
+        fig.set_figheight(2.5)
+        fig.set_figwidth(8)
+        fig.savefig(str(self.histograms_dir / plot_filename_hist), dpi=dpi)
+        plt.close(fig)
+
+
+        f.write(f"<img src='differences/{plot_filename_diff}'>\n")
+        f.write(f"<img src='histograms/{plot_filename_hist}'>\n")
 
         f.flush()
+
+        return rel_max_error, rel_mean, rel_stddev
 
 
 def main(argv):
@@ -132,6 +223,7 @@ def main(argv):
     args = cli.parse(parser, args=argv)
 
     reporter = Reporter(args.dir)
+    stats_dict = dict()
     try:
         with safe_open(args.expected_path, framework="pt") as exp_f, safe_open(
             args.actual_path, framework="pt"
@@ -141,8 +233,11 @@ def main(argv):
             for name in exp_keys:
                 if name not in act_keys:
                     continue
-                reporter.compare(name, exp_f.get_tensor(name), act_f.get_tensor(name))
+                fixed_name = name.replace("#", "_") # fixes urls in html
+                stats = reporter.compare(fixed_name, exp_f.get_tensor(name), act_f.get_tensor(name))
+                stats_dict[name] = stats
     finally:
+        reporter.overall_graph(stats_dict)
         reporter.close()
 
 
