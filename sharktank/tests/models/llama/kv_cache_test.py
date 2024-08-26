1e223bb4f927e71008e04b377f7387c1c53811ef
aviator19941 <avinash.sharma@amd.com>
2024-08-26 10:30:36 -0500

    Fix tests and add asserts to check key and value caches match
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/models/llama/kv_cache_test.py
+++ sharktank/tests/models/llama/kv_cache_test.py
@@ -8,12 +8,12 @@ from sharktank.models.llama.llama import (
 )
 from sharktank.models.llama.testing import *
 from sharktank.layers.rotary_embedding import RotaryEmbeddingLayer
-from ...layers import *
+from sharktank.layers import causal_llm
 
 
 class KVCacheTest(unittest.TestCase):
     def setUp(self):
-        self.block_count = 1
+        self.block_count = 5
         self.seq_len = 16
         self.head_count = 32
         self.head_dim = 128
@@ -44,7 +44,7 @@ class KVCacheTest(unittest.TestCase):
             transformer_block_count=self.head_count,
             attn_head_count=self.head_count,
             attn_head_dim=self.head_dim,
-            seq_length=self.seq_len,
+            seq_length=self.max_seq_len,
             device="cpu",
             dtype=torch.float32,
         )
@@ -84,85 +84,88 @@ class KVCacheTest(unittest.TestCase):
                 for n in range(self.block_count)
             ]
         )
-
-    def testDirectAndPagedKVCachePrefill(self):
-        torch.set_default_dtype(torch.float32)
-
-        paged_cache_state = self.paged_kv_cache.allocate(page_count=128)
-        paged_seq_block_ids = torch.tensor(
+        self.paged_cache_state = self.paged_kv_cache.allocate(page_count=128)
+        self.paged_seq_block_ids = torch.tensor(
             [
                 [127],
             ]
         )
-        direct_cache_state = self.direct_kv_cache.allocate(bs=1)
-        direct_seq_block_ids = torch.tensor(
+        self.direct_cache_state = self.direct_kv_cache.allocate(bs=1)
+        self.direct_seq_block_ids = torch.tensor(
             [
                 [0],
             ]
         )
+        self.embedding_batch_mask = self.attention_embedding.compute_batch_mask(
+            self.start_positions, batch_seq_len=1
+        )
+        self.model = causal_llm.BaseCausalLMModel(
+            self.attention_block_theta, context_length=4096
+        )
+        self.prefill_attention_mask = self.model.attention_mask(
+            self.model.input_mask(self.start_positions, self.seq_len)
+        )
+
+    def testDirectAndPagedKVCachePrefill(self):
+        torch.set_default_dtype(torch.float32)
 
         paged_input_tensor = make_rand_torch(
             (1, self.seq_len, self.head_count * self.head_dim), dtype=torch.float32
         )
         direct_input_tensor = paged_input_tensor.detach().clone()
-
         # Iterate over paged attention blocks.
         for block_idx, paged_block in enumerate(self.paged_attn_blocks):
-            if block_idx != 0:
-                paged_input_tensor = paged_block(
-                    paged_input_tensor,
-                    embedding=self.attention_embedding,
-                    start_index=0,
-                    cache_state=self.paged_cache_state,
-                    seq_block_ids=paged_seq_block_ids,
-                )
+            paged_input_tensor = paged_block(
+                paged_input_tensor,
+                embedding=self.attention_embedding,
+                start_index=0,
+                attention_mask=self.prefill_attention_mask,
+                cache_state=self.paged_cache_state,
+                seq_block_ids=self.paged_seq_block_ids,
+            )
         # Iterate over direct attention blocks.
         for block_idx, direct_block in enumerate(self.direct_attn_blocks):
-            if block_idx != 0:
-                direct_input_tensor = direct_block(
-                    direct_input_tensor,
-                    embedding=self.attention_embedding,
-                    start_index=0,
-                    cache_state=direct_cache_state,
-                    seq_block_ids=direct_seq_block_ids,
-                )
+            direct_input_tensor = direct_block(
+                direct_input_tensor,
+                embedding=self.attention_embedding,
+                start_index=0,
+                attention_mask=self.prefill_attention_mask,
+                cache_state=self.direct_cache_state,
+                seq_block_ids=self.direct_seq_block_ids,
+            )
+        page_table = self.paged_kv_cache.unflatten_page_table(self.paged_cache_state)
+        index_written = self.start_positions.item()
+        page_id = self.paged_seq_block_ids[0][0].item()
+        updated_direct_cache_state = self.direct_cache_state[0][
+            :, :index_written
+        ].squeeze(0)
+        updated_paged_cache_state = page_table[page_id][0, 0, :index_written]
+        assert updated_direct_cache_state.shape == updated_paged_cache_state.shape
+        torch.testing.assert_close(
+            updated_direct_cache_state, updated_paged_cache_state
+        )
+
         paged_prefill_attn_output = paged_input_tensor
         direct_prefill_attn_output = direct_input_tensor
-
         assert paged_prefill_attn_output.shape == direct_prefill_attn_output.shape
         torch.testing.assert_close(
             paged_prefill_attn_output, direct_prefill_attn_output
         )
 
-    @unittest.skip(
-        "Attention weights are matching, but attention output does not match, need to look into it more"
-    )
     def testDirectAndPagedKVCacheDecode(self):
         torch.set_default_dtype(torch.float32)
-
-        paged_cache_state = self.paged_kv_cache.allocate(page_count=128)
-        print(paged_cache_state)
-        paged_seq_block_ids = torch.tensor(
-            [
-                [127],
-            ]
-        )
-        direct_cache_state = self.direct_kv_cache.allocate(bs=1)
-        direct_seq_block_ids = torch.tensor(
-            [
-                [0],
-            ]
+        self.start_positions.add_(1)
+        assert self.direct_seq_block_ids.shape[1] == self.paged_seq_block_ids.shape[1]
+        self.decode_attention_mask = self.model.decode_attention_mask(
+            self.model.input_mask(
+                self.start_positions, self.direct_seq_block_ids.shape[1] * self.seq_len
+            )
         )
 
         token_paged_input_tensor = make_rand_torch(
             (1, 1, self.head_count * self.head_dim), dtype=torch.float32
         )
-        print(token_paged_input_tensor)
         token_direct_input_tensor = token_paged_input_tensor.detach().clone()
-
-        embedding_batch_mask = self.attention_embedding.compute_batch_mask(
-            self.start_positions, batch_seq_len=1
-        )
 
         xk_temp = torch.empty(
             [
@@ -185,36 +188,16 @@ class KVCacheTest(unittest.TestCase):
             device="cpu",
         )
 
-        values = [
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            float("-inf"),
-            float("-inf"),
-            float("-inf"),
-            float("-inf"),
-            float("-inf"),
-            float("-inf"),
-            float("-inf"),
-        ]
-        attention_mask = torch.tensor(values).view(1, 1, 1, 16)
-
         # Iterate over paged attention blocks.
         for block_idx, paged_block in enumerate(self.paged_attn_blocks):
             token_paged_input_tensor = paged_block(
                 token_paged_input_tensor,
                 start_positions=self.start_positions,
                 embedding=self.attention_embedding,
-                embedding_batch_mask=embedding_batch_mask,
-                attention_mask=attention_mask,
-                cache_state=paged_cache_state,
-                seq_block_ids=paged_seq_block_ids,
+                embedding_batch_mask=self.embedding_batch_mask,
+                attention_mask=self.decode_attention_mask,
+                cache_state=self.paged_cache_state,
+                seq_block_ids=self.paged_seq_block_ids,
                 xk_temp=xk_temp,
                 xv_temp=xv_temp,
             )
@@ -225,18 +208,42 @@ class KVCacheTest(unittest.TestCase):
                 token_direct_input_tensor,
                 start_positions=self.start_positions,
                 embedding=self.attention_embedding,
-                embedding_batch_mask=embedding_batch_mask,
-                attention_mask=attention_mask,
-                cache_state=direct_cache_state,
-                seq_block_ids=direct_seq_block_ids,
+                embedding_batch_mask=self.embedding_batch_mask,
+                attention_mask=self.decode_attention_mask,
+                cache_state=self.direct_cache_state,
+                seq_block_ids=self.direct_seq_block_ids,
                 xk_temp=xk_temp,
                 xv_temp=xv_temp,
             )
 
+        page_table = self.paged_kv_cache.unflatten_page_table(self.paged_cache_state)
+        index_written = self.start_positions.item()
+        page_id = self.paged_seq_block_ids[0][0].item()
+        updated_direct_cache_state_keys = self.direct_cache_state[0][
+            :, index_written
+        ].squeeze(0)
+        updated_paged_cache_state_keys = page_table[page_id][0, 0, index_written]
+        updated_direct_cache_state_values = self.direct_cache_state[1][
+            :, index_written
+        ].squeeze(0)
+        updated_paged_cache_state_values = page_table[page_id][0, 1, index_written]
+        assert (
+            updated_direct_cache_state_keys.shape
+            == updated_paged_cache_state_keys.shape
+        )
+        torch.testing.assert_close(
+            updated_direct_cache_state_keys, updated_paged_cache_state_keys
+        )
+        assert (
+            updated_direct_cache_state_values.shape
+            == updated_paged_cache_state_values.shape
+        )
+        torch.testing.assert_close(
+            updated_direct_cache_state_values, updated_paged_cache_state_values
+        )
+
         paged_decode_attn_output = token_paged_input_tensor
         direct_decode_attn_output = token_direct_input_tensor
-        print("paged:", paged_decode_attn_output)
-        print("direct:", direct_decode_attn_output)
         assert paged_decode_attn_output.shape == direct_decode_attn_output.shape
         torch.testing.assert_close(paged_decode_attn_output, direct_decode_attn_output)
 
