7e054b7dd37b74380e398dd69e858394bfe135a4
Stella Laurenzo <stellaraccident@gmail.com>
2024-09-03 16:11:31 -0700

    Add options for static/dynamic build and disable static

--- libshortfin/build_tools/cmake/shortfin_library.cmake
+++ libshortfin/build_tools/cmake/shortfin_library.cmake
@@ -24,20 +24,24 @@ function(shortfin_public_library)
     "COMPONENTS"
     ${ARGN}
   )
-  shortfin_components_to_static_libs(_STATIC_COMPONENTS ${_RULE_COMPONENTS})
-  shortfin_components_to_dynamic_libs(_DYLIB_COMPONENTS ${_RULE_COMPONENTS})
-  # Static library.
-  add_library("${_RULE_NAME}-static" STATIC)
-  target_link_libraries(
-    "${_RULE_NAME}-static" PUBLIC ${_STATIC_COMPONENTS}
-  )
+  if(SHORTFIN_BUILD_STATIC)
+    # Static library.
+    shortfin_components_to_static_libs(_STATIC_COMPONENTS ${_RULE_COMPONENTS})
+    add_library("${_RULE_NAME}-static" STATIC)
+    target_link_libraries(
+      "${_RULE_NAME}-static" PUBLIC ${_STATIC_COMPONENTS}
+    )
+  endif()
 
-  # Dylib library.
-  add_library("${_RULE_NAME}" SHARED)
-  target_compile_definitions("${_RULE_NAME}" INTERFACE _SHORTFIN_USING_DYLIB)
-  target_link_libraries(
-    "${_RULE_NAME}" PUBLIC ${_DYLIB_COMPONENTS}
-  )
+  if(SHORTFIN_BUILD_DYNAMIC)
+    # Dylib library.
+    shortfin_components_to_dynamic_libs(_DYLIB_COMPONENTS ${_RULE_COMPONENTS})
+    add_library("${_RULE_NAME}" SHARED)
+    target_compile_definitions("${_RULE_NAME}" INTERFACE _SHORTFIN_USING_DYLIB)
+    target_link_libraries(
+      "${_RULE_NAME}" PUBLIC ${_DYLIB_COMPONENTS}
+    )
+  endif()
 endfunction()
 
 function(shortfin_cc_component)
@@ -48,50 +52,52 @@ function(shortfin_cc_component)
     "HDRS;SRCS;DEPS;COMPONENTS"
     ${ARGN}
   )
-  set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-  set(_STATIC_OBJECTS_NAME "${_RULE_NAME}.objects")
-  set(_DYLIB_OBJECTS_NAME "${_RULE_NAME}.dylib.objects")
+  if(SHORTFIN_BUILD_STATIC)
+    # Static object library.
+    set(_STATIC_OBJECTS_NAME "${_RULE_NAME}.objects")
+    shortfin_components_to_static_libs(_STATIC_COMPONENTS ${_RULE_COMPONENTS})
+    add_library(${_STATIC_OBJECTS_NAME} OBJECT)
+    target_sources(${_STATIC_OBJECTS_NAME}
+      PRIVATE
+        ${_RULE_SRCS}
+        ${_RULE_HDRS}
+    )
+    target_compile_options(${_STATIC_OBJECTS_NAME} PRIVATE ${SHORTFIN_DEFAULT_COPTS})
+    target_link_libraries(${_STATIC_OBJECTS_NAME}
+      PUBLIC
+        _shortfin_defs
+        ${_STATIC_COMPONENTS}
+        ${_RULE_DEPS}
+    )
+  endif()
 
-  shortfin_components_to_static_libs(_STATIC_COMPONENTS ${_RULE_COMPONENTS})
-  shortfin_components_to_dynamic_libs(_DYLIB_COMPONENTS ${_RULE_COMPONENTS})
-
-  # Static object library.
-  add_library(${_STATIC_OBJECTS_NAME} OBJECT)
-  target_sources(${_STATIC_OBJECTS_NAME}
-    PRIVATE
-      ${_RULE_SRCS}
-      ${_RULE_HDRS}
-  )
-  target_compile_options(${_STATIC_OBJECTS_NAME} PRIVATE ${SHORTFIN_DEFAULT_COPTS})
-  target_link_libraries(${_STATIC_OBJECTS_NAME}
-    PUBLIC
-      _shortfin_defs
-      ${_STATIC_COMPONENTS}
-      ${_RULE_DEPS}
-  )
-
-  # Dylib object library.
-  add_library(${_DYLIB_OBJECTS_NAME} OBJECT)
-  target_sources(${_DYLIB_OBJECTS_NAME}
-    PRIVATE
-      ${_RULE_SRCS}
-      ${_RULE_HDRS}
-  )
-  target_compile_options(${_DYLIB_OBJECTS_NAME} PRIVATE ${SHORTFIN_DEFAULT_COPTS})
-  target_link_libraries(${_DYLIB_OBJECTS_NAME}
-    PUBLIC
-      _shortfin_defs
-      ${_DYLIB_COMPONENTS}
-      ${_RULE_DEPS}
-  )
-  set_target_properties(
-    ${_DYLIB_OBJECTS_NAME} PROPERTIES
-    CXX_VISIBILITY_PRESET hidden
-    C_VISIBILITY_PRESET hidden
-    VISIBILITY_INLINES_HIDDEN ON
-  )
-  target_compile_definitions(${_DYLIB_OBJECTS_NAME}
-    PRIVATE _SHORTFIN_BUILDING_DYLIB)
+  if(SHORTFIN_BUILD_DYNAMIC)
+    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+    set(_DYLIB_OBJECTS_NAME "${_RULE_NAME}.dylib.objects")
+    shortfin_components_to_dynamic_libs(_DYLIB_COMPONENTS ${_RULE_COMPONENTS})
+    # Dylib object library.
+    add_library(${_DYLIB_OBJECTS_NAME} OBJECT)
+    target_sources(${_DYLIB_OBJECTS_NAME}
+      PRIVATE
+        ${_RULE_SRCS}
+        ${_RULE_HDRS}
+    )
+    target_compile_options(${_DYLIB_OBJECTS_NAME} PRIVATE ${SHORTFIN_DEFAULT_COPTS})
+    target_link_libraries(${_DYLIB_OBJECTS_NAME}
+      PUBLIC
+        _shortfin_defs
+        ${_DYLIB_COMPONENTS}
+        ${_RULE_DEPS}
+    )
+    set_target_properties(
+      ${_DYLIB_OBJECTS_NAME} PROPERTIES
+      CXX_VISIBILITY_PRESET hidden
+      C_VISIBILITY_PRESET hidden
+      VISIBILITY_INLINES_HIDDEN ON
+    )
+    target_compile_definitions(${_DYLIB_OBJECTS_NAME}
+      PRIVATE _SHORTFIN_BUILDING_DYLIB)
+  endif()
 endfunction()
 
 function(shortfin_components_to_static_libs out_static_libs)
@@ -122,7 +128,7 @@ function(shortfin_gtest_test)
   add_executable(${_RULE_NAME} ${_RULE_SRCS})
   target_link_libraries(${_RULE_NAME} PRIVATE
     ${_RULE_DEPS}
-    shortfin
+    ${SHORTFIN_LINK_LIBRARY_NAME}
     GTest::gmock
     GTest::gtest_main
   )
