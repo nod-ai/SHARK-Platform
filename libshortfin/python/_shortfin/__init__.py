82a3a80da7495b55904083e8289ca659016bdb2b
Kunwar Grover <groverkss@gmail.com>
2024-09-12 12:23:46 +0100

    pre-commit

--- libshortfin/python/_shortfin/__init__.py
+++ libshortfin/python/_shortfin/__init__.py
@@ -18,10 +18,12 @@ if variant == "tracy":
         from _shortfin_tracy import lib
     except ModuleNotFoundError as e:
         raise ModuleNotFoundError(
-                "Shortfin Tracy runtime requested via SHORTFIN_PY_RUNTIME but it is not enabled in this build"
+            "Shortfin Tracy runtime requested via SHORTFIN_PY_RUNTIME but it is not enabled in this build"
         )
     print("-- Using Tracy runtime (SHORTFIN_PY_RUNTIME=tracy)", file=sys.stderr)
 else:
     if variant != "default":
-        warnings.warn(f"Unknown value for SHORTFIN_PY_RUNTIME env var ({variant}): Using default")
+        warnings.warn(
+            f"Unknown value for SHORTFIN_PY_RUNTIME env var ({variant}): Using default"
+        )
     from _shortfin_default import lib
