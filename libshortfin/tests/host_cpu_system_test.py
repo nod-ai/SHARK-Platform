f2e2ffb6688869c360153239cc8def299e671646
Stella Laurenzo <stellaraccident@gmail.com>
2024-09-25 16:59:39 -0700

    Test

--- libshortfin/tests/host_cpu_system_test.py
+++ libshortfin/tests/host_cpu_system_test.py
@@ -30,7 +30,7 @@ def clean_env():
 def test_create_host_cpu_system_defaults():
     sc = sf.host.CPUSystemBuilder()
     with sc.create_system() as ls:
-        print(f"LOCAL SYSTEM:", ls)
+        print(f"DEFAULT LOCAL SYSTEM:", ls)
         print("\n".join(repr(d) for d in ls.devices))
         assert len(ls.devices) > 0
 
@@ -40,7 +40,7 @@ def test_create_host_cpu_system_topology_nodes_all():
         hostcpu_topology_nodes="all", hostcpu_topology_max_group_count=2
     )
     with sc.create_system() as ls:
-        print(f"LOCAL SYSTEM:", ls)
+        print(f"NODES ALL LOCAL SYSTEM:", ls)
         print("\n".join(repr(d) for d in ls.devices))
         assert len(ls.devices) > 0
 
@@ -50,7 +50,7 @@ def test_create_host_cpu_system_topology_nodes_explicit():
         hostcpu_topology_nodes="0,0", hostcpu_topology_max_group_count=2
     )
     with sc.create_system() as ls:
-        print(f"LOCAL SYSTEM:", ls)
+        print(f"NODES EXPLICIT LOCAL SYSTEM:", ls)
         print("\n".join(repr(d) for d in ls.devices))
         assert len(ls.devices) == 2
 
@@ -60,7 +60,7 @@ def test_create_host_cpu_system_env_vars():
     os.putenv("SHORTFIN_HOSTCPU_TOPOLOGY_MAX_GROUP_COUNT", "2")
     sc = sf.host.CPUSystemBuilder()
     with sc.create_system() as ls:
-        print(f"LOCAL SYSTEM:", ls)
+        print(f"ENV VARS LOCAL SYSTEM:", ls)
         print("\n".join(repr(d) for d in ls.devices))
         assert len(ls.devices) == 2
 
