9c168f365aacbe305c376fd26d580d56f1e328ce
archana-ramalingam <archana.ramalingam@amd.com>
2024-10-01 14:36:43 -0500

    Add timing wrapper

--- sharktank/tests/evaluate/perplexity.py
+++ sharktank/tests/evaluate/perplexity.py
@@ -4,6 +4,9 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+import logging
+import time
+
 import json
 import numpy as np
 from tqdm import tqdm
@@ -23,6 +26,10 @@ from sharktank.models.grok.grok import *
 from sharktank.utils import cli
 from sharktank.utils.load_llm import *
 
+logging.basicConfig()
+logger = logging.getLogger("eval")
+logger.setLevel(logging.INFO)
+
 
 class Perplexity:
     """
@@ -36,12 +43,34 @@ class Perplexity:
     def __init__(
         self,
         prompts: list,
+        device,
     ):
         self.prompts = prompts
         self.add_start_token = False
         self.batch_size = 16
         self.bs = len(prompts)
+        self.device = device
 
+    def timeit(func):
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            time_taken = end - start
+            time_str = "seconds"
+            if time_taken < 1:
+                time_taken = (end - start) * 1000
+                time_str = "ms"
+
+            func_name = func.__name__
+            if func_name == "get_perplexity":
+                func_name = "Total time"
+            logger.info(f" {func_name}: {time_taken} {time_str}")
+            return result
+
+        return wrapper
+
+    @timeit
     def load_model(self, dataset, tokenizer):
 
         theta = dataset.root_theta
@@ -50,7 +79,7 @@ class Perplexity:
             hp=configs.LlamaHParams.from_gguf_props(dataset.properties),
             block_seq_stride=16,
             kv_cache_type=args.kv_cache_type,
-            device=device,
+            device=self.device,
             activation_dtype=torch.float16,
             attention_dtype=torch.float16,
         )
@@ -65,6 +94,7 @@ class Perplexity:
 
         self.generator = TorchGenerator(model, tokenizer)
 
+    @timeit
     def get_logits(self):
 
         token_ids, seq_lens = self.generator.tokenizer.encode(
@@ -73,33 +103,33 @@ class Perplexity:
             add_start_token=self.add_start_token,
         )
 
-        print(f"\n\n:: Prompts:")
+        logger.info(f" Prompts:")
         for idx, prompt in enumerate(self.prompts):
-            print(f"    {prompt.encode()}")
-            print(f"    {token_ids[idx]}\n")
+            logger.info(f" {prompt.encode()}\n{token_ids[idx]}")
 
         max_prompt_length = max(seq_lens)
 
-        self.token_ids = torch.tensor(token_ids, device=self.generator.model.device)
-        self.attention_mask = torch.tensor((self.token_ids != 0).int())
+        self.token_ids = torch.tensor(token_ids, device=self.device)
+        self.attention_mask = (self.token_ids != 0).int().detach().clone()
 
         is_first_token = True
         for i in tqdm(
             range(0, max_prompt_length),
-            desc="eval-perplexity: Load models & Fetching logits",
+            desc="eval: Calculating logits",
         ):
             token_batch = self.token_ids[:, : i + 1]
-            seq_lens_batch = torch.tensor([i] * self.bs)
-            # print('token_batch\n', token_batch, self.generator.tokenizer.decode(token_batch))
+            logger.debug(f"Iteration: {i}")
+            logger.debug(
+                f"Input tokens: {self.generator.tokenizer.decode(token_batch)}"
+            )
 
             token_batch, seq_lens_batch = self.generator.tokenizer.pad_tokens(
                 token_ids=token_batch.tolist(),
                 pad_to_multiple_of=self.generator.model.cache.pad_sequence_stride,
             )
-            token_batch = torch.tensor(token_batch, device=self.generator.model.device)
-            seq_lens_batch = torch.tensor(
-                seq_lens_batch, device=self.generator.model.device
-            )
+
+            token_batch = torch.tensor(token_batch, device=self.device)
+            seq_lens_batch = torch.tensor(seq_lens_batch, device=self.device)
 
             self.batch = self.generator.begin_eval_batch(
                 token_batch=token_batch,
@@ -108,7 +138,7 @@ class Perplexity:
             )
 
             self.cache_state = self.batch.prefill()
-            # print(self.batch.detokenize())
+            logger.debug(f"Prefill predicted token: {self.batch.detokenize()}")
 
             if is_first_token:
                 self.out_logits = self.batch.prefill_logits[:, 0:1, :]
@@ -121,36 +151,15 @@ class Perplexity:
             if i == max_prompt_length - 1:
                 self.pad_logits = self.batch.prefill_logits[:, i + 1 :, :]
 
-            # self.cache_state = self.batch.decode(self.cache_state)
-            # print(f":: Result tokens: {self.batch.results}")
-            # self.batch.print_current_results()
-            # self.out_logits = torch.cat((self.out_logits, self.batch.decode_logits), 1)
+        self.out_logits = torch.cat((self.out_logits, self.pad_logits), 1).to(
+            self.device
+        )
 
-        # print('out_logits', self.out_logits.shape, self.pad_logits.shape, self.token_ids.shape)
-        # shape_diff = self.token_ids.shape[1] - self.out_logits.shape[1]
-        # self.pad_logits = torch.cat([self.pad_logits] * shape_diff, 1)
-        self.out_logits = torch.cat((self.out_logits, self.pad_logits), 1)
-        # print('out_logits', self.out_logits.shape, self.pad_logits.shape, self.token_ids.shape)
-
+    @timeit
     def compute_perplexity(self):
-
-        self.get_logits()
-
         loss_fct = CrossEntropyLoss(reduction="none")
 
-        self.out_logits = self.out_logits[..., :-1, :].contiguous()
-        self.token_ids = self.token_ids[..., 1:].contiguous()
-        self.attention_mask = self.attention_mask[..., 1:].contiguous()
-
-        # print('shift3', self.out_logits[0,0,:20], self.out_logits.shape)
-        # print('shift4', self.token_ids, self.token_ids.shape)
-        # print('shift7', self.attention_mask, self.attention_mask.shape)
-
-        assert self.token_ids.shape == self.out_logits.shape[0:2]
-
-        # torch.save([self.out_logits, self.token_ids, self.attention_mask], '/home/aramalin/sharktank/logits_perplexity_sharktank.pt')
-
-        # perplexity = e ^ (sum(losses) / num_tokenized_tokens)
+        ## perplexity = e ^ (sum(losses) / num_tokenized_tokens)
         crossentropy_loss = (
             loss_fct(self.out_logits.transpose(1, 2), self.token_ids)
             * self.attention_mask
@@ -160,37 +169,53 @@ class Perplexity:
             crossentropy_loss / self.attention_mask.sum(1)
         ).tolist()
 
-        perplexity = dict(map(lambda i, j: (i, j), self.prompts, perplexity_batch))
-
         return {
-            "perplexities": perplexity,
+            "perplexities": perplexity_batch,
             "mean_perplexity": np.mean(perplexity_batch),
         }
+
+    @timeit
+    def get_perplexity(self):
+
+        self.get_logits()
+
+        self.out_logits = self.out_logits[..., :-1, :].contiguous()
+        self.token_ids = self.token_ids[..., 1:].contiguous()
+        self.attention_mask = self.attention_mask[..., 1:].contiguous()
+
+        assert self.token_ids.shape == self.out_logits.shape[0:2]
+
+        logger.debug(f"Logits shape: {self.out_logits.shape}")
+        logger.debug(f"Token ids: {self.token_ids}, {self.token_ids.shape}")
+        logger.debug(
+            f"Logits shape: {self.attention_mask}, {self.attention_mask.shape}"
+        )
+
+        return self.compute_perplexity()
 
 
 def run_perplexity(
     prompts: list[str],
     dataset,
     tokenizer,
+    device,
 ):
-    perplexity = Perplexity(prompts=prompts)
+    perplexity = Perplexity(prompts=prompts, device=device)
 
     perplexity.load_model(dataset, tokenizer)
-    ppl = perplexity.compute_perplexity()
+    ppl = perplexity.get_perplexity()
 
     return ppl
 
 
 if __name__ == "__main__":
     parser = cli.create_parser()
-    # parser.add_argument("-p", "--prompt", nargs='+', help="Prompt strings", required=True)
     parser.add_argument("--kv-cache-type", default="paged", help="KV cache type")
     parser.add_argument("--device", help="Torch device (or default)")
 
     cli.add_input_dataset_options(parser)
     cli.add_tokenizer_options(parser)
     args = cli.parse(parser)
-    # input_texts = args.prompt
 
     device = torch.device(args.device) if args.device else None
 
@@ -204,8 +229,9 @@ if __name__ == "__main__":
         'Richard Gale "Dick" Rifenburg (August 21, 1926-December 5, 1994) was an American football player and a pioneering television broadcaster for the forerunner to WIVB-TV in Buffalo. He played college football for the University of Michigan Wolverines in 1944 and from 1946 to 1948. He was a consensus selection at end on the 1948 College Football All-America Team. Rifenburg played professionally in the National Football League (NFL) with the Detroit Lions for one season in 1950. After retiring from football he settled in Buffalo and became a sports broadcaster.',
         "An oxaziridine is an organic molecule that features a three-membered heterocycle containing oxygen, nitrogen, and carbon. In their largest application, oxazidines are intermediates in the industrial production of hydrazine. Oxaziridine derivatives are also used as specialized reagents in organic chemistry for a variety of oxidations, including alpha hydroxylation of enolates, epoxidation and aziridination of olefins, and other heteroatom transfer reactions.",
     ]
-    input_texts = ["Happy Birthday!", "Write a story about LLamas"]
 
-    ppl = run_perplexity(prompts=input_texts, dataset=dataset, tokenizer=tokenizer)
+    ppl = run_perplexity(
+        prompts=input_texts[:3], dataset=dataset, tokenizer=tokenizer, device=device
+    )
 
-    print(json.dumps(ppl, indent=2))
+    logger.info(f"\n{json.dumps(ppl, indent=2)}")
