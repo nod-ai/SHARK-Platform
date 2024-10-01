b6d60e084ad8125858247192fd295e6b818f83ae
Scott Todd <scott.todd0@gmail.com>
2024-10-01 14:37:41 -0700

    [shortfin] Fix mobilenet_server example by writing bytes directly.

--- shortfin/examples/python/mobilenet_server/inference_system.py
+++ shortfin/examples/python/mobilenet_server/inference_system.py
@@ -40,7 +40,12 @@ class InferenceProcess(sf.Process):
             # just writing to the backing storage is the best we have API
             # support for. Generally, APIs on storage should be mirrored onto
             # the array.
-            self.host_staging.storage.data = request.raw_image_data
+            # TODO: Easier to use API for writing into the storage
+            with self.host_staging.storage.map(write=True, discard=True) as m:
+                request_bytes = request.raw_image_data.tobytes()
+                mv = memoryview(m)
+                for i in range(0, len(request_bytes)):
+                    mv[i] = request_bytes[i]
             print("host_staging =", self.host_staging)
             self.device_input.copy_from(self.host_staging)
 
