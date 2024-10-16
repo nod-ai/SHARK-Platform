3264ecd66129d9176184ff3267449b689b592d69
aviator19941 <avinash.sharma@amd.com>
2024-10-16 10:56:50 -0500

    Add f16 non-decomposed and fp8 decomposed and non-decomposed tests
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/models/llama/benchmark-tests.py
+++ sharktank/tests/models/llama/benchmark-tests.py
@@ -7,6 +7,7 @@
 import logging
 import os
 import unittest
+import pytest
 import subprocess
 from pathlib import Path
 from typing import List
@@ -17,7 +18,11 @@ class BaseBenchmarkTest(unittest.TestCase):
         raise NotImplementedError("Subclasses should implement this method.")
 
     def get_export_cmd(
-        self, irpa_path: str, output_mlir_path: str, output_json_path: str
+        self,
+        attention_kernel: str,
+        irpa_path: str,
+        output_mlir_path: str,
+        output_json_path: str,
     ):
         export_args = [
             "python3",
@@ -30,6 +35,10 @@ class BaseBenchmarkTest(unittest.TestCase):
             "--output-config",
             output_json_path,
         ]
+        if attention_kernel in ["decomposed", "torch_sdpa"]:
+            export_args.append("--attention-kernel")
+            export_args.append(attention_kernel)
+
         cmd = subprocess.list2cmdline(export_args)
         return cmd
 
@@ -42,6 +51,7 @@ class BaseBenchmarkTest(unittest.TestCase):
 
     def export_mlir(
         self,
+        attention_kernel: str,
         irpa_path: str,
         output_mlir_path: str,
         output_json_path: str,
@@ -53,7 +63,9 @@ class BaseBenchmarkTest(unittest.TestCase):
             output_mlir_path: Path to the file to save the exported file.
             output_json_path: Path to the file to save the config json file.
         """
-        cmd = self.get_export_cmd(irpa_path, output_mlir_path, output_json_path)
+        cmd = self.get_export_cmd(
+            attention_kernel, irpa_path, output_mlir_path, output_json_path
+        )
         logging.getLogger().info(f"Launching export command:\n" f"cd {cwd} && {cmd}")
         proc = subprocess.run(cmd, shell=True, capture_output=True, cwd=cwd)
         return_code = proc.returncode
@@ -121,9 +133,13 @@ class BenchmarkLlama3_1_8B_f16(BaseBenchmarkTest):
         self.repo_root = os.getenv("SHARK_PLATFORM_REPO_ROOT")
         artifacts_dir = "/data/extra/models/llama3.1_8B/"
         self.irpa_path = artifacts_dir + "llama8b_f16.irpa"
+        self.irpa_path_fp8 = artifacts_dir + "llama8b_fp8.irpa"
         self.output_mlir = self.repo_root + "llama8b_f16.mlir"
         self.output_json = self.repo_root + "llama8b_f16.json"
         self.output_vmfb = self.repo_root + "llama8b_f16.vmfb"
+        self.output_mlir_fp8 = self.repo_root + "llama8b_fp8.mlir"
+        self.output_json_fp8 = self.repo_root + "llama8b_fp8.json"
+        self.output_vmfb_fp8 = self.repo_root + "llama8b_fp8.vmfb"
         self.iree_compile_args = [
             "--iree-hal-target-backends=rocm",
             "--iree-hip-target=gfx942",
@@ -131,6 +147,8 @@ class BenchmarkLlama3_1_8B_f16(BaseBenchmarkTest):
         ]
         self.prefill_args_f16 = artifacts_dir + "prefill_args"
         self.decode_args_f16 = artifacts_dir + "decode_args"
+        self.prefill_args_fp8 = artifacts_dir + "prefill_args_fp8"
+        self.decode_args_fp8 = artifacts_dir + "decode_args_fp8"
         self.iree_run_prefill_args = [
             "--function=prefill_bs4",
             f"--input=@{self.prefill_args_f16}/tokens.npy",
@@ -148,10 +166,60 @@ class BenchmarkLlama3_1_8B_f16(BaseBenchmarkTest):
             f"--input=@{self.decode_args_f16}/cache_state_f16.npy",
             "--benchmark_repetitions=3",
         ]
+        self.iree_run_prefill_args_fp8 = [
+            "--function=prefill_bs4",
+            f"--input=@{self.prefill_args_fp8}/tokens.npy",
+            f"--input=@{self.prefill_args_fp8}/seq_lens.npy",
+            f"--input=@{self.prefill_args_fp8}/seq_block_ids.npy",
+            f"--input=@{self.prefill_args_fp8}/cache_state_f16.npy",
+            "--benchmark_repetitions=3",
+        ]
+        self.iree_run_decode_args_fp8 = [
+            "--function=decode_bs4",
+            f"--input=@{self.decode_args_fp8}/tokens.npy",
+            f"--input=@{self.decode_args_fp8}/seq_lens.npy",
+            f"--input=@{self.decode_args_fp8}/start_positions.npy",
+            f"--input=@{self.decode_args_fp8}/seq_block_ids.npy",
+            f"--input=@{self.decode_args_fp8}/cache_state_f16.npy",
+            "--benchmark_repetitions=3",
+        ]
 
-    def testBenchmark8B_f16(self):
+    def testBenchmark8B_f16_Decomposed(self):
         self.export_mlir(
-            self.irpa_path, self.output_mlir, self.output_json, self.repo_root
+            "decomposed",
+            self.irpa_path,
+            self.output_mlir,
+            self.output_json,
+            self.repo_root,
+        )
+        self.iree_compile(
+            self.output_mlir, self.output_vmfb, self.iree_compile_args, self.repo_root
+        )
+        # benchmark prefill
+        self.iree_benchmark_module(
+            "0",
+            self.output_vmfb_fp8,
+            self.irpa_path_fp8,
+            self.iree_run_prefill_args_fp8,
+            self.repo_root,
+        )
+        # benchmark decode
+        self.iree_benchmark_module(
+            "0",
+            self.output_vmfb_fp8,
+            self.irpa_path_fp8,
+            self.iree_run_decode_args_fp8,
+            self.repo_root,
+        )
+
+    @pytest.mark.xfail
+    def testBenchmark8B_f16_Non_Decomposed(self):
+        self.export_mlir(
+            "torch_sdpa",
+            self.irpa_path,
+            self.output_mlir,
+            self.output_json,
+            self.repo_root,
         )
         self.iree_compile(
             self.output_mlir, self.output_vmfb, self.iree_compile_args, self.repo_root
@@ -170,6 +238,70 @@ class BenchmarkLlama3_1_8B_f16(BaseBenchmarkTest):
             self.output_vmfb,
             self.irpa_path,
             self.iree_run_decode_args,
+            self.repo_root,
+        )
+
+    @pytest.mark.xfail
+    def testBenchmark8B_fp8_Decomposed(self):
+        self.export_mlir(
+            "decomposed",
+            self.irpa_path_fp8,
+            self.output_mlir_fp8,
+            self.output_json_fp8,
+            self.repo_root,
+        )
+        self.iree_compile(
+            self.output_mlir_fp8,
+            self.output_vmfb_fp8,
+            self.iree_compile_args,
+            self.repo_root,
+        )
+        # benchmark prefill
+        self.iree_benchmark_module(
+            "0",
+            self.output_vmfb,
+            self.irpa_path,
+            self.iree_run_prefill_args,
+            self.repo_root,
+        )
+        # benchmark decode
+        self.iree_benchmark_module(
+            "0",
+            self.output_vmfb,
+            self.irpa_path,
+            self.iree_run_decode_args,
+            self.repo_root,
+        )
+
+    @pytest.mark.xfail
+    def testBenchmark8B_fp8_Non_Decomposed(self):
+        self.export_mlir(
+            "torch_sdpa",
+            self.irpa_path_fp8,
+            self.output_mlir_fp8,
+            self.output_json_fp8,
+            self.repo_root,
+        )
+        self.iree_compile(
+            self.output_mlir_fp8,
+            self.output_vmfb_fp8,
+            self.iree_compile_args,
+            self.repo_root,
+        )
+        # benchmark prefill
+        self.iree_benchmark_module(
+            "0",
+            self.output_vmfb_fp8,
+            self.irpa_path_fp8,
+            self.iree_run_prefill_args_fp8,
+            self.repo_root,
+        )
+        # benchmark decode
+        self.iree_benchmark_module(
+            "0",
+            self.output_vmfb_fp8,
+            self.irpa_path_fp8,
+            self.iree_run_decode_args_fp8,
             self.repo_root,
         )
 
