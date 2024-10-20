2964fffa6e72d37b6cd4f6eb598c7d8a809e223b
Kyle Herndon <kherndon@amd.com>
2024-10-20 17:09:53 -0500

    Fix test

--- sharktank/tests/layers/paged_llama_attention_block_test.py
+++ sharktank/tests/layers/paged_llama_attention_block_test.py
@@ -15,7 +15,7 @@ import torch
 from iree.turbine import aot
 from sharktank.layers import PagedLlamaAttentionBlock, PagedKVCache, RotaryEmbeddingLayer
 from sharktank.layers.testing import make_llama_attention_block_theta
-
+from sharktank.types.tensors import DefaultPrimitiveTensor
 
 class PagedLlamaAttentionBlockTest(unittest.TestCase):
     def setUp(self):
@@ -41,7 +41,6 @@ class PagedLlamaAttentionBlockTest(unittest.TestCase):
 
     def testExportDecomposed(self):
         dtype = torch.float32
-        torch.set_default_dtype(dtype)
 
         cache = PagedKVCache(
             transformer_block_count=self.transformer_block_count,
@@ -51,6 +50,9 @@ class PagedLlamaAttentionBlockTest(unittest.TestCase):
             block_seq_stride=self.block_seq_stride,
             dtype=dtype,
         )
+
+        cache_state = cache.paged.allocate(self.page_count)
+        cache_state[0] = torch.rand(cache_state[0].shape, dtype=dtype)
 
         theta = make_llama_attention_block_theta(
             head_count=self.attention_head_count,
@@ -68,36 +70,105 @@ class PagedLlamaAttentionBlockTest(unittest.TestCase):
             rms_epsilon=self.rms_epsilon,
             attention_kernel="decomposed",
         )
+
         seq_block_ids = torch.arange(self.batch_size * self.block_seqlen).view(
             self.batch_size, -1
         )
+        
         embedding_module = RotaryEmbeddingLayer(
             rope_dimension_count=self.rope_dimension_count,
             max_seqlen=self.max_seqlen,
             rope_freq_base=self.rope_freq_base,
         )
+        
         class MyModule(torch.nn.Module):
-            def forward(self, h):
-                return attn.forward(h, seq_block_ids=seq_block_ids, embedding=embedding_module)
+            def forward(self, h, seq_block_ids, cache_state):
+                return attn.forward(h, seq_block_ids=seq_block_ids, embedding=embedding_module, start_index=0, cache_state=cache_state)
+        
         mod = MyModule()
-        #fxb = aot.FxProgramsBuilder(mod)
-        #@fxb.export_program(
-        #        name="forward",
-        #        args=(torch.rand([4,16,320]),),
-        #        dynamic_shapes={})
-        #def _(model, *args, **kwargs) -> torch.Tensor:
-        #    return model.forward(*args, **kwargs)
+        h = torch.rand([self.batch_size, self.max_seqlen, self.attention_head_count * self.attention_head_dim])
+        mod.forward(h,
+                seq_block_ids,
+                cache_state)
         ep = torch.export.export(
             mod,
             args=(
-                torch.rand([4, 16, 320], dtype=torch.float32),
+                h,
+                seq_block_ids,
+                cache_state,
             ),
         )
-        output = aot.export(fxb)
-        #output = aot.export(ep)
+        output = aot.export(ep)
         output.verify()
         asm = str(output.mlir_module)
+        output.save_mlir("temp.mlir")
         self.assertNotIn("scaled_dot_product_attention", asm)
+
+
+    def testExportDecomposed(self):
+        dtype = torch.float32
+
+        cache = PagedKVCache(
+            transformer_block_count=self.transformer_block_count,
+            attn_head_count=self.head_count_kv,
+            attn_head_dim=self.attention_head_dim,
+            cache_partition_count=self.cache_partition_count,
+            block_seq_stride=self.block_seq_stride,
+            dtype=dtype,
+        )
+
+        cache_state = cache.paged.allocate(self.page_count)
+        cache_state[0] = torch.rand(cache_state[0].shape, dtype=dtype)
+
+        theta = make_llama_attention_block_theta(
+            head_count=self.attention_head_count,
+            head_count_kv=self.head_count_kv,
+            head_dim=self.attention_head_dim,
+            embedding_length=self.embedding_length,
+        )
+        attn = PagedLlamaAttentionBlock(
+            theta=theta,
+            block_index=self.block_index,
+            cache=cache,
+            head_count=self.attention_head_count,
+            head_dim=self.attention_head_dim,
+            head_count_kv=self.head_count_kv,
+            rms_epsilon=self.rms_epsilon,
+            attention_kernel="torch",
+        )
+
+        seq_block_ids = torch.arange(self.batch_size * self.block_seqlen).view(
+            self.batch_size, -1
+        )
+        
+        embedding_module = RotaryEmbeddingLayer(
+            rope_dimension_count=self.rope_dimension_count,
+            max_seqlen=self.max_seqlen,
+            rope_freq_base=self.rope_freq_base,
+        )
+        
+        class MyModule(torch.nn.Module):
+            def forward(self, h, seq_block_ids, cache_state):
+                return attn.forward(h, seq_block_ids=seq_block_ids, embedding=embedding_module, start_index=0, cache_state=cache_state)
+        
+        mod = MyModule()
+        h = torch.rand([self.batch_size, self.max_seqlen, self.attention_head_count * self.attention_head_dim])
+        mod.forward(h,
+                seq_block_ids,
+                cache_state)
+        ep = torch.export.export(
+            mod,
+            args=(
+                h,
+                seq_block_ids,
+                cache_state,
+            ),
+        )
+        output = aot.export(ep)
+        output.verify()
+        asm = str(output.mlir_module)
+        output.save_mlir("temp.mlir")
+        self.assertIn("torch.aten._scaled_dot_product_flash_attention_for_cpu", asm)
 
 
 if __name__ == "__main__":
