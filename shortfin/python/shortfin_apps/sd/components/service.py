22cc9defd6d2eb4faaf4cfe4bc70cd9ebd045d2e
Ean Garvey <87458719+monorimet@users.noreply.github.com>
2024-10-27 18:41:49 -0500

    Remove unused streaming attribute.

--- shortfin/python/shortfin_apps/sd/components/service.py
+++ shortfin/python/shortfin_apps/sd/components/service.py
@@ -51,7 +51,6 @@ class GenerateService:
         self.inference_functions: dict[str, dict[str, sf.ProgramFunction]] = {}
         self.inference_programs: dict[str, sf.Program] = {}
         self.procs_per_device = 1
-        self.streaming = True
         self.workers = []
         self.fibers = []
         self.locks = []
