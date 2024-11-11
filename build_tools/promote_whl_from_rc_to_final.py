b9dbb96520d55833ff0e35032efc499c72be41ef
Scott Todd <scott.todd0@gmail.com>
2024-11-11 12:03:28 -0800

    Update copyright.
    (I'm the sole author and wrote the code to use in both projects)

--- build_tools/promote_whl_from_rc_to_final.py
+++ build_tools/promote_whl_from_rc_to_final.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2024 The IREE Authors
+# Copyright 2024 Advanced Micro Devices, Inc.
 #
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
