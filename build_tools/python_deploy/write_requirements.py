5fe07931b9f55709f5f81dacb9298e6543303e1c
Marius Brehler <marius.brehler@amd.com>
2024-11-11 23:11:56 +0100

    Adjust writing requirements files

--- build_tools/python_deploy/write_requirements.py
+++ build_tools/python_deploy/write_requirements.py
@@ -17,16 +17,24 @@
 # * shortfin
 # get pinned to the corresponding `X.Y.*` version.
 
-
+import argparse
 from pathlib import Path
 import json
 
 from packaging.version import Version
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--version-suffix", action="store", type=str)
+
+args = parser.parse_args()
+
+
 THIS_DIR = Path(__file__).parent.resolve()
 REPO_ROOT = THIS_DIR.parent.parent
 
+VERSION_FILE_SHARKTANK = REPO_ROOT / "sharktank/version_info.json"
+VERSION_FILE_SHORTFIN = REPO_ROOT / "shortfin/version_info.json"
 VERSION_FILE_LOCAL = REPO_ROOT / "packaging/shark-ai/version_local.json"
 REQUIREMENTS_TXT = REPO_ROOT / "packaging/shark-ai/requirements.txt"
 
@@ -43,20 +51,33 @@ def write_requirements(package_list, package_version):
             f.write("%s\n" % PINNED_PACKAGE)
 
 
+def append_requirements(package_list, package_version):
+    with open(REQUIREMENTS_TXT, "a") as f:
+        for package in package_list:
+            PINNED_PACKAGE = package + "==" + package_version
+            f.write("%s\n" % PINNED_PACKAGE)
+
+
 metapackage_version = load_version_info(VERSION_FILE_LOCAL)
 PACKAGE_VERSION = metapackage_version.get("package-version")
 
-nightly_packages_list = ["sharktank", "shortfin"]
-stable_packages_list = [
-    "iree-base-compiler",
-    "iree-base-runtime",
-    "iree-turbine",
-    "sharktank",
-    "shortfin",
-]
+sharktank_version = load_version_info(VERSION_FILE_SHARKTANK)
+SHARKTANK_PACKAGE_VERSION = sharktank_version.get("package-version")
+
+shortfin_version = load_version_info(VERSION_FILE_SHORTFIN)
+SHORTFIN_PACKAGE_VERSION = shortfin_version.get("package-version")
+
+stable_packages_list = ["iree-base-compiler", "iree-base-runtime", "iree-turbine"]
 
 if Version(PACKAGE_VERSION).is_prerelease:
-    write_requirements(nightly_packages_list, PACKAGE_VERSION)
+    write_requirements(
+        ["sharktank"],
+        Version(SHARKTANK_PACKAGE_VERSION).base_version + "rc" + args.version_suffix,
+    )
+    append_requirements(
+        ["shortfin"],
+        Version(SHORTFIN_PACKAGE_VERSION).base_version + "rc" + args.version_suffix,
+    )
 else:
     MAJOR_VERSION = Version(PACKAGE_VERSION).major
     MINOR_VERSION = Version(PACKAGE_VERSION).minor
@@ -64,3 +85,5 @@ else:
     write_requirements(
         stable_packages_list, str(MAJOR_VERSION) + "." + str(MINOR_VERSION) + ".*"
     )
+    append_requirements(["sharktank"], Version(SHARKTANK_PACKAGE_VERSION).base_version)
+    append_requirements(["shortfin"], Version(SHORTFIN_PACKAGE_VERSION).base_version)
