adc67cdc21d0dd6955adf53a8064a6ae389cc769
Marius Brehler <marius.brehler@amd.com>
2024-11-12 15:06:48 +0100

    Refactor `write_requirements.py`
    Refactors the script to generate the requirements file in memory and
    write only once.

--- build_tools/python_deploy/write_requirements.py
+++ build_tools/python_deploy/write_requirements.py
@@ -44,18 +44,9 @@ def load_version_info(version_file):
         return json.load(f)
 
 
-def write_requirements(package_list, package_version):
+def write_requirements(requirements):
     with open(REQUIREMENTS_TXT, "w") as f:
-        for package in package_list:
-            PINNED_PACKAGE = package + "==" + package_version
-            f.write("%s\n" % PINNED_PACKAGE)
-
-
-def append_requirements(package_list, package_version):
-    with open(REQUIREMENTS_TXT, "a") as f:
-        for package in package_list:
-            PINNED_PACKAGE = package + "==" + package_version
-            f.write("%s\n" % PINNED_PACKAGE)
+        f.write("%s\n" % requirements)
 
 
 metapackage_version = load_version_info(VERSION_FILE_LOCAL)
@@ -70,20 +61,34 @@ SHORTFIN_PACKAGE_VERSION = shortfin_version.get("package-version")
 stable_packages_list = ["iree-base-compiler", "iree-base-runtime", "iree-turbine"]
 
 if Version(PACKAGE_VERSION).is_prerelease:
-    write_requirements(
-        ["sharktank"],
-        Version(SHARKTANK_PACKAGE_VERSION).base_version + "rc" + args.version_suffix,
+    requirements = (
+        "sharktank=="
+        + Version(SHARKTANK_PACKAGE_VERSION).base_version
+        + "rc"
+        + args.version_suffix
+        + "\n"
     )
-    append_requirements(
-        ["shortfin"],
-        Version(SHORTFIN_PACKAGE_VERSION).base_version + "rc" + args.version_suffix,
+    requirements += (
+        "shortfin=="
+        + Version(SHORTFIN_PACKAGE_VERSION).base_version
+        + "rc"
+        + args.version_suffix
     )
+
+    write_requirements(requirements)
+
 else:
     MAJOR_VERSION = Version(PACKAGE_VERSION).major
     MINOR_VERSION = Version(PACKAGE_VERSION).minor
 
-    write_requirements(
-        stable_packages_list, str(MAJOR_VERSION) + "." + str(MINOR_VERSION) + ".*"
+    STABLE_VERSION_TO_PIN = str(MAJOR_VERSION) + "." + str(MINOR_VERSION) + ".*"
+
+    requirements = ""
+    for package in stable_packages_list:
+        requirements += package + "==" + STABLE_VERSION_TO_PIN + "\n"
+    requirements += (
+        "sharktank==" + Version(SHARKTANK_PACKAGE_VERSION).base_version + "\n"
     )
-    append_requirements(["sharktank"], Version(SHARKTANK_PACKAGE_VERSION).base_version)
-    append_requirements(["shortfin"], Version(SHORTFIN_PACKAGE_VERSION).base_version)
+    requirements += "shortfin==" + Version(SHORTFIN_PACKAGE_VERSION).base_version
+
+    write_requirements(requirements)
