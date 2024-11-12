8500a04cd5067bbe6cc6732cd4cb2ef764642bed
Cedar <cedar.ren@gmail.com>
2024-11-12 10:45:11 -0800

    minor readability changes to trigger another integration test to see if caching worked

--- build_tools/integration_tests/llm/cpu_llm_server_test.py
+++ build_tools/integration_tests/llm/cpu_llm_server_test.py
@@ -66,14 +66,17 @@ def do_generate(prompt, port):
     "model_test_dir,llm_server",
     [
         (
-            {
+            {  # FixtureRequest for model_test_dir
                 "repo_id": "SlyEcho/open_llama_3b_v2_gguf",
                 "model_file": "open-llama-3b-v2-f16.gguf",
                 "tokenizer_id": "openlm-research/open_llama_3b_v2",
                 "settings": CPU_SETTINGS,
                 "batch_sizes": [1, 4],
             },
-            {"model_file": "open-llama-3b-v2-f16.gguf", "settings": CPU_SETTINGS},
+            {  # FixtureRequest for llm_server
+                "model_file": "open-llama-3b-v2-f16.gguf",
+                "settings": CPU_SETTINGS,
+            },
         )
     ],
     indirect=True,
