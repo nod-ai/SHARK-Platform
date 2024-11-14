bfc7738a8895d88a2edadf7dcadc7e87cd265d70
Xida Ren (Cedar) <cedar.ren@gmail.com>
2024-11-14 14:16:48 -0500

    Revert "Rework kv cache to avoid needless reshaping (#450)" (#509)
    This reverts commit b36ddf7842e7124926b5f99d89e504bcbef93753.
    
    The beginning of the fix to the brand new November KV cache issue

--- sharktank/sharktank/layers/paged_llama_attention_block.py
+++ sharktank/sharktank/layers/paged_llama_attention_block.py
@@ -241,7 +241,7 @@ class PagedLlamaAttentionBlock(ThetaLayer):
         # Restore from the cache.
         xk, xv = cache.read(
             cache_state,
-            dest_partitions=[
+            read_into_partitions=[
                 xk_temp[:, 0:kv_seq_len, ...],
                 xv_temp[:, 0:kv_seq_len, ...],
             ],
