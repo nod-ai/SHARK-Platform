9535984ece971f69479a3714c351aa381ce5b7c2
Boian Petkantchin <boian.petkantchin@amd.com>
2024-11-20 13:31:58 -0800

    Add T5 LM v1.1 encoder (#550)
    The encoder shares much of the underlying stack as the decoder. Here
    only the encoder is presented as a class.
    I have not gone out of my way to strip all decoder related stuff from
    the stack. Things like check-pointing and dropout are stripped.
    
    The author attribution is added to the license of the T5 model file as
    this seems like a derivative work. They are both Apache 2.0.
    
    There are a few tests of the various components and 2 tests for the
    entire encoder for the small and xxl variants. They relay on huggingface
    and the models are downloaded no the fly into the cache. The tests
    expect the corresponding GGUF files to be already preset and available
    on the file system.

--- sharktank/conftest.py
+++ sharktank/conftest.py
@@ -88,6 +88,16 @@ def pytest_addoption(parser):
         help="Enable all llama benchmarking tests",
     )
 
+    parser.addoption(
+        "--with-t5-data",
+        action="store_true",
+        default=False,
+        help=(
+            "Enable tests that use T5 data like models that is not a part of the source "
+            "code. The user is expected to provide the data"
+        ),
+    )
+
     # TODO: Remove all hardcoded paths in CI tests
     parser.addoption(
         "--llama3-8b-tokenizer-path",
@@ -131,6 +141,28 @@ def pytest_addoption(parser):
         action="store",
         default=None,
         help="Llama3.1 405b fp8 model path",
+    )
+
+    # To obtain a T5 GGUF file you can use llama.cpp's convert_hf_to_gguf.py.
+    # https://github.com/ggerganov/llama.cpp/blob/9abe9eeae98b11fa93b82632b264126a010225ff/convert_hf_to_gguf.py
+    # E.g.
+    # git lfs install
+    # git clone https://huggingface.co/google/t5-v1_1-small
+    # convert_hf_to_gguf.py \
+    #     --outfile t5-v1_1-small.gguf \
+    #     --outtype=f32 \
+    #     t5-v1_1-small
+    parser.addoption(
+        "--google-t5-v1-1-small-fp32-model-path",
+        type=Path,
+        default="/data/t5/small/google__t5-v1_1-small_fp32.gguf",
+        help="Google T5 v1.1 small fp32 model path",
+    )
+    parser.addoption(
+        "--google-t5-v1-1-xxl-fp32-model-path",
+        type=Path,
+        default="/data/t5/xxl/google__t5-v1_1-xxl_fp32.gguf",
+        help="Google T5 v1.1 XXL fp32 model path",
     )
 
     parser.addoption(
@@ -255,6 +287,16 @@ def get_model_artifacts(request: FixtureRequest):
     )
     model_path["llama3_405b_fp8_model_path"] = set_fixture_from_cli_option(
         request, "--llama3-405b-fp8-model-path", "llama3_405b_fp8_model"
+    )
+    model_path["google__t5_v1_1_small_fp32_model_path"] = set_fixture_from_cli_option(
+        request,
+        "--google-t5-v1-1-small-fp32-model-path",
+        "google__t5_v1_1_small_fp32_model",
+    )
+    model_path["google__t5_v1_1_xxl_fp32_model_path"] = set_fixture_from_cli_option(
+        request,
+        "--google-t5-v1-1-xxl-fp32-model-path",
+        "google__t5_v1_1_xxl_fp32_model",
     )
     return model_path
 
