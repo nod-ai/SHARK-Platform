90ad6075350acc319f15996397262f9174234bb5
aviator19941 <avinash.sharma@amd.com>
2024-11-21 12:11:17 -0600

    Update testBenchmark405B_f16_TP8_Non_Decomposed to Benchmarking Error
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- sharktank/tests/models/llama/benchmark_amdgpu_test.py
+++ sharktank/tests/models/llama/benchmark_amdgpu_test.py
@@ -779,7 +779,9 @@ class BenchmarkLlama3_1_405B(BaseBenchmarkTest):
             cwd=self.repo_root,
         )
 
-    @pytest.mark.xfail(reason="Compile Error", strict=True, raises=IreeCompileException)
+    @pytest.mark.xfail(
+        reason="Benchmarking Error", strict=True, raises=IreeBenchmarkException
+    )
     def testBenchmark405B_f16_TP8_Non_Decomposed(self):
         output_file_name = self.dir_path_405b / "f16_torch"
         output_mlir = self.llama405b_f16_torch_sdpa_artifacts.create_file(
