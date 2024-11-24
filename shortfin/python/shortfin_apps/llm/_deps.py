33240628a12277f5d93c0782bfeda8b18b65bc4d
Xida Ren <xidaren2@amd.com>
2024-11-24 06:12:04 +0000

    skip when dependencies not found in a way compatible with shortfin

--- shortfin/python/shortfin_apps/llm/_deps.py
+++ shortfin/python/shortfin_apps/llm/_deps.py
@@ -5,13 +5,20 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from shortfin.support.deps import ShortfinDepNotFoundError
+import sys
 
-try:
-    import tokenizers
-except ModuleNotFoundError as e:
-    raise ShortfinDepNotFoundError(__name__, "tokenizers") from e
+shortfin_llm_deps = [
+    "tokenizers",
+    "dataclasses_json",
+]
 
-try:
-    import dataclasses_json
-except ModuleNotFoundError as e:
-    raise ShortfinDepNotFoundError(__name__, "dataclasses-json") from e
+for dep in shortfin_llm_deps:
+    try:
+        __import__(dep)
+    except ModuleNotFoundError as e:
+        if "pytest" in sys.modules:
+            import pytest
+
+            pytest.skip(f"Shortfin LLM dependency not available: {dep}")
+        else:
+            raise ShortfinDepNotFoundError(__name__, dep) from e
