af4d84b21e5a9a89cf1b8b0da7e1567d707afa3e
Cedar <cedar.ren@gmail.com>
2024-11-27 10:34:19 -0800

    another small fix for efficiency. Thanks stephen!

--- shortfin/python/shortfin_apps/llm/components/messages.py
+++ shortfin/python/shortfin_apps/llm/components/messages.py
@@ -58,8 +58,8 @@ class InferenceExecRequest(sf.Message):
     def cache_page_indices(self, max_len: int) -> list[int]:
         if not self.allocation:
             return []
-        indices = [p.index for p in self.allocation.pages]
-        return indices[:max_len]
+        indices = [p.index for p in self.allocation.pages[:max_len]]
+        return indices
 
     def publish_allocated_pages(self, up_to_page_index: int):
         assert self.allocation
