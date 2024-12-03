cd0293a82a2e377b870b3ad0ce63214c8ef33bd6
Cedar <cedar.ren@gmail.com>
2024-12-03 12:28:49 -0800

    simplify config handling via consistency between sharktank and shortfin

--- app_tests/integration_tests/llm/shortfin/conftest.py
+++ app_tests/integration_tests/llm/shortfin/conftest.py
@@ -72,23 +72,6 @@ def model_test_dir(request, tmp_path_factory):
         vmfb_path = tmp_dir / "model.vmfb"
         compile_model(mlir_path, vmfb_path, settings)
 
-        # Write config
-        edited_config_path = tmp_dir / "edited_config.json"
-        config = {
-            "module_name": "module",
-            "module_abi_version": 1,
-            "max_seq_len": 2048,
-            "attn_head_count": 32,
-            "attn_head_dim": 100,
-            "prefill_batch_sizes": batch_sizes,
-            "decode_batch_sizes": batch_sizes,
-            "transformer_block_count": 26,
-            "paged_kv_cache": {"block_seq_stride": 16, "device_block_count": 256},
-        }
-        logger.info(f"Saving edited config to: {edited_config_path}\n")
-        logger.info(f"Config: {json.dumps(config, indent=2)}")
-        with open(edited_config_path, "w") as f:
-            json.dump(config, f)
         logger.info("Model artifacts setup successfully" + end_log_group())
         yield hf_home, tmp_dir
     finally:
@@ -120,7 +103,7 @@ def llm_server(request, model_test_dir, available_port):
     settings = request.param["settings"]
 
     tokenizer_path = hf_home / "tokenizer.json"
-    config_path = tmp_dir / "edited_config.json"
+    config_path = tmp_dir / "config.json"
     vmfb_path = tmp_dir / "model.vmfb"
     parameters_path = hf_home / model_file
 
