539be41f32b4fe693a5a038ffb9099b74e358b57
Stephen Baione <109226581+stbaione@users.noreply.github.com>
2024-12-04 11:51:28 -0600

    Add Baseline for SGLang Benchmark Test (#602)
    # Description
    
    The SGLang Benchmark Test has been running for awhile, but only
    benchmarks the shortfin server itself. In order to get a baseline metric
    and enable long-term convergence in-terms of performance, we need to be
    able to track metrics of the SGLang server using the same benchmark
    method.
    
    This adds an `sglang_benchmark_test` to complement the
    `shortfin_benchmark_test`. Also restructures
    `app_tests/benchmark_tests/llm` ->
    `app_tests/benchmark_tests/llm/sglang_benchmarks`. This keeps the
    benchmark tests organized and allows for the folder to be extended with
    other types of benchmarks in the future.
    
    # Why are we using docker to start the SGLang server?
    
    Currently, the pyprompt.toml file inside of SGLang requires
    `vllm==0.6.3.dev13` to run on ROCm. I looked into potentially building
    vLLM from source for this test, but couldn't find a branch, tag, or
    release that matched that signature. From their own comments inside of
    `pyproject.toml`, it appears to only be available inside of a `ROCm`
    base image:
    
    ```toml
    # HIP (Heterogeneous-computing Interface for Portability) for AMD
    # => base docker rocm/vllm-dev:20241022, not from public vllm whl
    srt_hip = ["sglang[runtime_common]", "torch", "vllm==0.6.3.dev13"]
    ```
    
    Their
    [instructions](https://sgl-project.github.io/start/install.html#method-3-using-docker)
    on installing SGLang and running for ROCm also appear to suggest the
    docker method:
    
    ## Instructions from their docs for running with ROCm
    
    ```
    docker build --build-arg SGL_BRANCH=v0.3.5.post2 -t v0.3.5.post2-rocm620 -f Dockerfile.rocm .
    
    alias drun='docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host \
        --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
        -v $HOME/dockerx:/dockerx -v /data:/data'
    
    drun -p 30000:30000 \
        -v ~/.cache/huggingface:/root/.cache/huggingface \
        --env "HF_TOKEN=<secret>" \
        v0.3.5.post2-rocm620 \
        python3 -m sglang.launch_server --model-path meta-llama/Llama-3.1-8B-Instruct --host 0.0.0.0 --port 30000
    ```
    
    The workflow file handles starting the container and cleaning up once
    the workflow is done. I set the timeout for waiting for the server to
    start to `10 minutes` to give the SGLang server enough time to load
    necessary model weights and startup.

--- app_tests/benchmark_tests/llm/sglang_benchmarks/sglang_benchmark_test.py
+++ app_tests/benchmark_tests/llm/sglang_benchmarks/sglang_benchmark_test.py
@@ -5,9 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import logging
-import multiprocessing
-import os
-from pathlib import Path
 import pytest
 import time
 from unittest.mock import patch
@@ -17,71 +14,32 @@ from sglang import bench_serving
 
 from .utils import SGLangBenchmarkArgs, log_jsonl_result
 
-from integration_tests.llm.utils import (
-    find_available_port,
-    start_llm_server,
-)
+from integration_tests.llm.utils import download_tokenizer, wait_for_server
 
 logger = logging.getLogger(__name__)
 
-device_settings = {
-    "device_flags": [
-        "--iree-hal-target-backends=rocm",
-        "--iree-hip-target=gfx942",
-    ],
-    "device": "hip",
-}
-
 
 @pytest.mark.parametrize(
-    "request_rate,model_param_file_name",
-    [
-        (req_rate, "meta-llama-3.1-8b-instruct.f16.gguf")
-        for req_rate in [1, 2, 4, 8, 16, 32]
-    ],
+    "request_rate,tokenizer_id",
+    [(req_rate, "NousResearch/Meta-Llama-3-8B") for req_rate in [1, 2, 4, 8, 16, 32]],
 )
-@pytest.mark.parametrize(
-    "pre_process_model",
-    [
-        (
-            {
-                "model_name": "llama3_8B_fp16",
-                "model_param_file_name": "meta-llama-3.1-8b-instruct.f16.gguf",
-                "settings": device_settings,
-                "batch_sizes": [1, 4],
-            }
-        )
-    ],
-    indirect=True,
-)
-def test_sglang_benchmark_server(
-    request_rate, model_param_file_name, pre_process_model
-):
-    # TODO: Remove when multi-device is fixed
-    os.environ["ROCR_VISIBLE_DEVICES"] = "1"
+def test_sglang_benchmark(request_rate, tokenizer_id, sglang_args, tmp_path_factory):
+    tmp_dir = tmp_path_factory.mktemp("sglang_benchmark_test")
 
-    tmp_dir = pre_process_model
+    # Download tokenizer for llama3_8B_fp16
+    download_tokenizer(tmp_dir, tokenizer_id)
 
-    config_path = tmp_dir / "config.json"
-    vmfb_path = tmp_dir / "model.vmfb"
-    tokenizer_path = tmp_dir / "tokenizer.json"
-    model_path = tmp_dir / model_param_file_name
+    logger.info("Beginning SGLang benchmark test...")
 
-    # Start shortfin llm server
-    port = find_available_port()
-    server_process = start_llm_server(
-        port,
-        tokenizer_path,
-        config_path,
-        vmfb_path,
-        model_path,
-        device_settings,
-        timeout=30,
-    )
+    port = sglang_args
+    base_url = f"http://localhost:{port}"
 
-    # Run and collect SGLang Serving Benchmark
+    # Setting a high timeout gives enough time for downloading model artifacts
+    # and starting up server... Takes a little longer than shortfin.
+    wait_for_server(base_url, timeout=600)
+
     benchmark_args = SGLangBenchmarkArgs(
-        backend="shortfin",
+        backend="sglang",
         num_prompt=10,
         base_url=f"http://localhost:{port}",
         tokenizer=tmp_dir,
@@ -95,21 +53,15 @@ def test_sglang_benchmark_server(
 
     logger.info("Running SGLang Benchmark with the following args:")
     logger.info(benchmark_args)
+
     try:
         start = time.time()
         with patch.object(bench_serving, "print", side_effect=logger.info):
-            benchmark_process = multiprocessing.Process(
-                target=bench_serving.run_benchmark,
-                args=(benchmark_args.as_namespace(),),
+            bench_serving.run_benchmark(
+                benchmark_args.as_namespace(),
             )
-            benchmark_process.start()
-            benchmark_process.join()
-
         logger.info(f"Benchmark run completed in {str(time.time() - start)} seconds")
         logger.info("======== RESULTS ========")
         log_jsonl_result(benchmark_args.output_file)
     except Exception as e:
         logger.error(e)
-
-    server_process.terminate()
-    server_process.wait()
