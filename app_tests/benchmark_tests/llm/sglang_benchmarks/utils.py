61c40ddca4a446a35aa7ace78c2591c543a06e19
Stephen Baione <stbaione@amd.com>
2024-12-07 00:17:39 +0000

    Raise error if one occurs, Patch SGLangBenchmarkArgs

--- app_tests/benchmark_tests/llm/sglang_benchmarks/utils.py
+++ app_tests/benchmark_tests/llm/sglang_benchmarks/utils.py
@@ -48,6 +48,7 @@ class SGLangBenchmarkArgs:
             disable_tqdm=False,
             disable_stream=False,
             disable_ignore_eos=False,
+            lora_name=None,
         )
 
     def __repr__(self):
