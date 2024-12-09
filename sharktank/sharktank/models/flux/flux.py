b6ac5dd12b2ee2e785ff676faa20cebe2bb4800a
Kyle Herndon <kherndon@amd.com>
2024-12-08 20:09:31 -0800

    Initial flux

---
+++ sharktank/sharktank/models/flux/flux.py
@@ -0,0 +1,290 @@
+# Copyright 2024 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import math
+from typing import Optional
+
+from dataclasses import dataclass
+from typing import Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ...layers import *
+from ...types import *
+from ...utils.create_cache import *
+from ... import ops
+
+__all__ = [
+    "FluxModelV1",
+]
+
+################################################################################
+# Models
+################################################################################
+
+
+@dataclass
+class FluxParams:
+    in_channels: int
+    out_channels: int
+    vec_in_dim: int
+    context_in_dim: int
+    hidden_size: int
+    mlp_ratio: float
+    num_heads: int
+    depth: int
+    depth_single_blocks: int
+    axes_dim: list[int]
+    theta: int
+    qkv_bias: bool
+    guidance_embed: bool
+
+
+class FluxModelV1(ThetaLayer):
+    """LlamaModel with a paged KV cache and supporting variable sequence
+    length batched inference.
+
+    As both the caching and batching setup is complicated, this model variant
+    is modular, intending to be instantiated and used in an overall assembly
+    vs trying to providing one-stop methods that do everything.
+
+    The inference procedure is typically:
+
+    1. Initialize the PagedKVCache state tensors.
+    2. Generate an input mask given a vector of sequence lengths.
+    3. Generate an attention mask from the input mask.
+    4. Allocate a block mapping table.
+    5. Invoke prefill() with a batch of sequences.
+    6. Extract tokens from batched logits.
+    7. Iteratively invoke decode() for as long as there are sequences needing
+       to be serviced.
+
+    Various samplers and schedulers can be interleaved throughout.
+
+    In the case of tensor sharding (config.tensor_parallelism_size > 1) the model's KV
+    cache head dimension is sharded.
+    The number of KV cache heads must be divisible by the parallelism size.
+    With this sharding approach the KV cache is not replicated across devices.
+    The cache is split across the devices while the indexing logic/computation is
+    replicated.
+    All other arguments aside from the cache state are replicated.
+    After the attention we all-reduce.
+    The the first fully connected layer is split along the parallel dimension.
+    This drives that the reduction dimension is split for the second FC layer.
+    We return the unreduced tensor. The user is free to reduce it to obtain the
+    unsharded result or chain it with other tensor-parallel operations.
+    """
+
+    def __init__(self, theta: Theta, params: FluxParams):
+        # hp = config.hp
+        super().__init__(
+            theta,
+        )
+        # self.config = config
+        # self.hp = hp
+        # self.cache = create_kv_cache(self.config)
+        # self.activation_dtype = config.activation_dtype
+        # self.use_hf = config.use_hf
+        # self.attention_kernel = config.attention_kernel
+
+        self.in_channels = params.in_channels
+        self.out_channels = self.in_channels
+        if params.hidden_size % params.num_heads != 0:
+            raise ValueError(
+                f"Hidden size {params.hidden_size} must be divisible by num_heads {params.num_heads}"
+            )
+        pe_dim = params.hidden_size // params.num_heads
+        if sum(params.axes_dim) != pe_dim:
+            raise ValueError(
+                f"Got {params.axes_dim} but expected positional dim {pe_dim}"
+            )
+        self.hidden_size = params.hidden_size
+        self.num_heads = params.num_heads
+        self.pe_embedder = EmbedND(
+            dim=pe_dim, theta=params.theta, axes_dim=params.axes_dim
+        )
+        self.add_module("img_in", LinearLayer(theta("img_in")))
+        # self.time_in = MLPEmbedder(in_dim=256, hidden_dim=self.hidden_size) # lin silu lin
+        self.add_module("time_in_0", LinearLayer(theta("time_in.0")))
+        self.add_module("time_in_1", LinearLayer(theta("time_in.1")))
+        # self.vector_in = MLPEmbedder(params.vec_in_dim, self.hidden_size)
+        self.add_module("vector_in_0", LinearLayer(theta("vector_in.0")))
+        self.add_module("vector_in_1", LinearLayer(theta("vector_in.1")))
+        self.guidance = False
+        if params.guidance_embed:
+            self.guidance = True
+            self.add_module("guidance_in_0", LinearLayer(theta("guidance_in.0")))
+            self.add_module("guidance_in_1", LinearLayer(theta("guidance_in.1")))
+        self.add_module("txt_in", LinearLayer(theta("txt_in")))
+
+        self.double_blocks = nn.ModuleList(
+            [
+                MMDITDoubleBlock(
+                    theta("double_blocks", i),
+                    self.num_heads,
+                )
+                for i in range(params.depth)
+            ]
+        )
+
+        self.single_blocks = nn.ModuleList(
+            [
+                MMDITSingleBlock(
+                    theta("single_blocks", i),
+                    self.num_heads,
+                )
+                for i in range(params.depth_single_blocks)
+            ]
+        )
+
+        self.add_module(
+            "last_layer",
+            LastLayer(theta("last_layer")),
+        )
+
+    def forward(
+        self,
+        img: AnyTensor,
+        img_ids: AnyTensor,
+        txt: AnyTensor,
+        txt_ids: AnyTensor,
+        timesteps: AnyTensor,
+        y: AnyTensor,
+        guidance: AnyTensor | None = None,
+    ) -> AnyTensor:
+        if img.ndim != 3 or txt.ndim != 3:
+            raise ValueError("Input img and txt tensors must have 3 dimensions.")
+
+        # running on sequences img
+        img = self.img_in(img)
+        time_in_0 = self.time_in_0(timestep_embedding(timesteps, 256))
+        time_in_silu = ops.elementwise(nn.SiLU(), time_in_0)
+        vec = self.time_in_1(time_in_silu)
+        if self.guidance:
+            if guidance is None:
+                raise ValueError(
+                    "Didn't get guidance strength for guidance distilled model."
+                )
+            guidance_inp = timestep_embedding(guidance, 256)
+            guidance0 = self.guidance_in0(guidance_inp)
+            guidance_silu = ops.elementwise(nn.SiLU(), guidance0)
+            guidance_out = self.guidance_in1(guidance_silu)
+            vec = vec + self.guidance_in(guidance_out)
+        vector_in_0 = self.vector_in_0(y)
+        vector_in_silu = ops.elementwise(nn.SiLU(), vector_in_0)
+        vector_in_1 = self.vector_in_1(vector_in_silu)
+        vec = vec + vector_in_1
+
+        txt = self.txt_in(txt)
+
+        ids = torch.cat((txt_ids, img_ids), dim=1)
+        pe = self.pe_embedder(ids)
+
+        for block in self.double_blocks:
+            img, txt = block(img=img, txt=txt, vec=vec, pe=pe)
+
+        img = torch.cat((txt, img), 1)
+        for block in self.single_blocks:
+            img = block(img, vec=vec, pe=pe)
+        img = img[:, txt.shape[1] :, ...]
+
+        img = self.final_layer(img, vec)  # (N, T, patch_size ** 2 * out_channels)
+        return img
+
+
+################################################################################
+# Layers
+################################################################################
+
+
+def timestep_embedding(
+    t: AnyTensor, dim, max_period=10000, time_factor: float = 1000.0
+):
+    """
+    Create sinusoidal timestep embeddings.
+    :param t: a 1-D Tensor of N indices, one per batch element.
+                      These may be fractional.
+    :param dim: the dimension of the output.
+    :param max_period: controls the minimum frequency of the embeddings.
+    :return: an (N, D) Tensor of positional embeddings.
+    """
+    t = time_factor * t
+    half = dim // 2
+    freqs = torch.exp(
+        -math.log(max_period)
+        * torch.arange(start=0, end=half, dtype=torch.float32)
+        / half
+    ).to(t.device)
+
+    args = t[:, None].float() * freqs[None]
+    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+    if dim % 2:
+        embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+    if torch.is_floating_point(t):
+        embedding = embedding.to(t)
+    return embedding
+
+
+def layer_norm(inp):
+    weight = torch.ones(inp.shape)
+    bias = torch.zeros(inp.shape)
+    return ops.layer_norm(inp, weight, bias, eps=1e-6)
+
+
+def qk_norm(q, k, v, rms_q, rms_k):
+    return rms_q(q).to(v), rms_k(k).to(v)
+
+
+def rope(pos: AnyTensor, dim: int, theta: int) -> AnyTensor:
+    assert dim % 2 == 0
+    scale = torch.arange(0, dim, 2, dtype=torch.float64, device=pos.device) / dim
+    omega = 1.0 / (theta**scale)
+    out = torch.einsum("...n,d->...nd", pos, omega)
+    out = torch.stack(
+        [torch.cos(out), -torch.sin(out), torch.sin(out), torch.cos(out)], dim=-1
+    )
+    # out = out.view(out.shape[0], out.shape[1], out.shape[2], out.shape[3], 2, 2)
+    out = out.view(out.shape[0], out.shape[1], out.shape[2], 2, 2)
+    return out.float()
+
+
+class EmbedND(torch.nn.Module):
+    def __init__(self, dim: int, theta: int, axes_dim: list[int]):
+        super().__init__()
+        self.dim = dim
+        self.theta = theta
+        self.axes_dim = axes_dim
+
+    def forward(self, ids: AnyTensor) -> AnyTensor:
+        n_axes = ids.shape[-1]
+        emb = torch.cat(
+            [rope(ids[..., i], self.axes_dim[i], self.theta) for i in range(n_axes)],
+            dim=-3,
+        )
+
+        return emb.unsqueeze(1)
+
+
+class LastLayer(ThetaLayer):
+    def __init__(
+        self,
+        theta: Theta,
+    ):
+        super().__init__(theta)
+        self.outlinear = self.add_module("outlinear", LinearLayer(theta("outlinear")))
+        self.ada_linear = self.add_module(
+            "ada_linear", LinearLayer(theta("ada_linear"))
+        )
+
+    def forward(self, x: AnyTensor, vec: AnyTensor) -> AnyTensor:
+        silu = ops.elementwise(nn.SiLU(), x)
+        lin = self.ada_linear(silu)
+        shift, scale = lin.chunk(2, dim=1)
+        x = (1 + scale[:, None, :]) * layer_norm(x) + shift[:, None, :]
+        x = self.out_linear(x)
+        return x
