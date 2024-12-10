b7f4f5ab3a4797f81336fdb6699d88a4d3b510f0
Maksim Levental <maksim.levental@gmail.com>
2024-12-10 08:48:16 -0800

    Update __init__.py

--- tuner/tuner/__init__.py
+++ tuner/tuner/__init__.py
@@ -3,3 +3,15 @@
 # Licensed under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from iree.compiler import ir
+
+# substitute replace=True so that colliding registration don't error
+def register_attribute_builder(kind, replace=True):
+    def decorator_builder(func):
+        AttrBuilder.insert(kind, func, replace=replace)
+        return func
+
+    return decorator_builder
+
+ir.register_attribute_builder = register_attribute_builder
